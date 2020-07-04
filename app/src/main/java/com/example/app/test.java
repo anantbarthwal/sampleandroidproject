00d9d0029186e9cb6a8f33b98b3a04d903c93de7
anantbarthwal <anantbarthwal@gmail.com>
2020-07-05 00:34:10 +0530

    test commit

---
+++ app/src/main/java/com/example/app/test.java
@@ -0,0 +1,13 @@
+package com.example.app;
+
+public class test {
+    private String ola;
+
+    public String getOla() {
+        return ola;
+    }
+
+    public void setOla(String ola) {
+        this.ola = ola;
+    }
+}
